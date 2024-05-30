5edfef8ba36015577b1b5fe24353b1555b544ebc
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-05-30 13:36:10 +0900

    컴포넌트완료

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,37 @@
+"use client"; //서버에서는 js를 랜더링할 수 없어서 클라이언트 브라우저에서 하기위해 ssr을 하는 next에서는 js는 x 따라서 csr하기 위해 클라이언트 설정
+import React, { useEffect } from 'react';
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Contact from "@/components/Contact";
+import Intro from "@/components/Intro";
+import Skill from "@/components/Skill";
+import Site from '@/components/Site';
+import Skip from '@/components/Skip';
+import Port from '@/components/Port';
+
+import lenis from '@/utils/lenis';
+import link from '@/utils/link';
 
 
 export default function Home() {
+  useEffect(() => {
+    lenis();
+    link();
+  },[]);
+
   return (
-    <div>next.js</div>
+    <>
+      <Skip/>
+      <Header/>
+      <main id='main'role="main">
+        <Intro/>
+        <Skill/>
+        <Site/>
+        <Port/>
+        <Contact/>
+      </main>
+      <Footer/>
+    </>
   );
 }
