de899e5f7cc406d89aa9c0dc9ed937ac6edca7dc
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-06-05 11:21:31 +0900

    사이트완료

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { siteText } from '@/constants'
 
 const Site = () => {
   return (
-    <div>
-        Site
+    <section id="site">
+    <div className="site_inner">
+        <h2 className="site_title">Site Coding <em>나의 작업물</em></h2>
+        <div className="site_wrap">
+            {siteText.map((site,index) => (
+              <article className={`site_item s${index+1}`} key={index}>
+                <span className='num'>{index+1}.</span>
+                <div className='text'>
+                  <div>{site.text[0]}</div>
+                  <div>{site.text[1]}</div>
+                  <div>{site.text[2]}</div>
+                </div>
+                <h3 className='title'>{site.title}</h3>
+                <div className='btn'>
+                  <a href={site.code}>code</a>
+                  <a href={site.view}>view</a>
+                </div>
+                <div className='info'>
+                  <span>{site.info[0]}</span>
+                  <span>{site.info[1]}</span>
+                  <span>{site.info[2]}</span>
+                </div>
+              </article>
+            )) }
+        </div>
     </div>
+</section>
   )
 }
 
