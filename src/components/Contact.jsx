20b122ba7ae236432068a01fcbc22bbd67dc5d91
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-06-10 14:52:30 +0900

    연락처영역완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 
+import { contactText } from '@/constants'
+
 const Contact = () => {
   return (
-    <div>
-        Contact
-    </div>
+    <section id="contact">
+        <div className="contact_inner">
+            <h2 className="contact_title">Contact</h2>
+            <div className="contact_lines top" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+            <div className="contact_text">
+                <div className="text">
+                    {contactText.map((contact, index)=>(
+                      <div key={index}>
+                        <a 
+                        href={contact.link}
+                        target="_blank"
+                        rel='noopener noreferrer'
+                        >
+                          {contact.title}
+                        </a>
+                      </div>
+                    ))}
+                </div>
+            </div>
+            <div className="contact_lines bottom" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+        </div>
+    </section>
   )
 }
 
